c7af94d991aae886f0bba2491d4374bfc7861bd7
akshaykolhe1989 <akshaykolhe1809@gmail.com>
2021-03-06 21:51:47 +0000

    Added decode404 - decode 404 exception intead of throwing FeignExceptions

--- customer-service/src/main/java/com/example/customer/OrderClient.java
+++ customer-service/src/main/java/com/example/customer/OrderClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "order-service", fallback = OrderClientFallback.class)
+@FeignClient(name = "order-service", decode404=true, fallback = OrderClientFallback.class)
 public interface OrderClient {
 	
 	@GetMapping("/")
