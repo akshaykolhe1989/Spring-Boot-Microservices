0d4be173d82bf1b46a063a8a20c7919638b7712e
akshaykolhe1989 <akshaykolhe1809@gmail.com>
2021-03-07 15:43:13 +0000

    Configured Feign (Internally uses Ribbon) to use Random Rule for Load balancing

--- customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
@@ -8,6 +8,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+
+/*
+* Adjust load balancing rule by openFeign (Internal uses Ribbon) in spring cloud. 
+* Below we have configured to use Random Rule for Load balancing within CloudProviderConfiguration class.
+* By default if not provided, openFeign (Internal uses Ribbon) uses Round Robbin Rule for Load balancing.
+*/
+//@RibbonClients({@RibbonClient(name="order-service", configuration = CloudProviderConfiguration.class)})  
+
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
